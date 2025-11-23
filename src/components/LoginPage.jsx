e36fd04f5d36af6861959cba28cfdeb61b051174
Yan Schober <yschober@google.com>
2025-11-23 08:50:10 -0800

    UI Polish: White background & Login glow

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -68,7 +68,7 @@ const LoginPage = () => {
           background: #07074cd4;
           padding: 3rem;
           border-radius: 16px;
-          box-shadow: 0 10px 25px rgba(0,0,0,0.2);
+          box-shadow: 0 10px 25px rgba(0,0,0,0.2), 0 0 30px rgba(255, 255, 255, 0.3);
           text-align: center;
           max-width: 400px;
           width: 90%;
